f7c275bcc08cc3ef60f85b3727b238f4ee32f756
Adam Kelly <adam.guitar12@gmail.com>
2018-12-08 21:26:52 +0000

    Added measurement functionality

--- gk.py
+++ gk.py
@@ -1,8 +1,9 @@
 from precomputed import *
+import random
+import itertools as it
 
 
 class GraphState(object):
-
     def __init__(self, num_qubits, **kwargs):
         self.adj = {}
         self.nodes = {}
@@ -79,13 +80,122 @@ class GraphState(object):
         edge = self._edge(a, b)
         vop_a = self.nodes[a]['vop']
         vop_b = self.nodes[b]['vop']
-        new_edge, self.nodes[a]['vop'], self.nodes[b]['vop'] = cphase_table[int(
-            edge), vop_a, vop_b]
+        new_edge, self.nodes[a]['vop'], self.nodes[b]['vop'] = cphase_table[
+            int(edge), vop_a, vop_b]
         if new_edge != edge:
             if self._edge(a, b):
                 self._remove_edge(a, b)
             else:
                 self._add_edge(a, b)
+
+    def _update_vop(self, v, op):
+        """Update a VOP"""
+        operation = vop_table[str(op)]
+        self.nodes[v]['vop'] = clifford_multiplication_table[
+            self.nodes[v]['vop'], operation]
+
+    def _measure_graph_y(self, node, result):
+        """Measure the bare graph in the Y-basis"""
+        for a in self.adj[node]:
+            self._update_vop(a, 'sqz' if result else 'msqz')
+
+        ngbg = set(self.adj[node]) | {node}
+        for i, j in it.combinations(ngbh, 2):
+            if self._edge(i, j):
+                self._remove_edge(i, j)
+            else:
+                self._add_edge(i, j)
+
+        self._update_vop(node, 5 if result else 6)
+        return result, False
+
+    def _measure_graph_z(self, node, result):
+        """Measure the bare graph in the Z-basis"""
+        # Disconnect
+        for neighbour in tuple(self.adj[node]):
+            self._del_edge(node, neighbour)
+            if result:
+                self._update_vop(neighbour, "pz")
+
+        # Rotate
+        if result:
+            self._update_vop(node, "px")
+            self._update_vop(node, "hadamard")
+        else:
+            self._update_vop(node, "hadamard")
+
+        return result, False
+
+    def _measure_graph_x(self, node, result, friend=None):
+        """ Measure the bare graph in the X-basis """
+        if len(self.adj[node]) == 0:
+            return 0, True
+
+        # Pick a friend vertex
+        if friend == None:
+            friend = next(iter(self.adj[node].keys()))
+        else:
+            assert friend in list(
+                self.adj[node].keys())  # TODO: unnecessary assert
+
+        # Update the VOPs. TODO: pretty ugly
+        if result:
+            # Do a z on all ngb(vb) \ ngb(v) \ {v}, and some other stuff
+            self._update_vop(friend, "msqy")
+            self._update_vop(node, "pz")
+
+            for n in set(self.adj[friend]) - set(self.adj[node]) - {node}:
+                self._update_vop(n, "pz")
+        else:
+            # Do a z on all ngb(v) \ ngb(vb) \ {vb}, and sqy on the friend
+            self._update_vop(friend, "sqy")
+            for n in set(self.adj[node]) - set(self.adj[friend]) - {friend}:
+                self._update_vop(n, "pz")
+
+        # Toggle the edges. TODO: Yuk. Just awful!
+        a = set(self.adj[node].keys())
+        b = set(self.adj[friend].keys())
+        if self._edge(a, b):
+            self._remove_edge(a, b)
+        else:
+            self._add_edge(a, b)
+        intersection = a & b
+        for i, j in it.combinations(intersection, 2):
+            if self._edge(i, j):
+                self._remove_edge(i, j)
+            else:
+                self._add_edge(i, j)
+
+        for n in a - {friend}:
+            if self._edge(friend, n):
+                self._remove_edge(friend, n)
+            else:
+                self._add_edge(friend, n)
+
+        return result, False
+
+    def _measure(self, target, basis):
+        basis = vop_table[basis]
+        ha = conjugation_table[self.nodes[target]['vop']]
+        basis, phase = conjugate(basis, ha)
+
+        # Make a random choice
+        result = random.choice([0, 1])
+
+        if basis == vop_table['px']:
+            result, determinate = self._measure_graph_x(target, result)
+        elif basis == vop_table['py']:
+            result, determinate = self._measure_graph_y(target, result)
+        elif basis == vop_table['pz']:
+            result, determintate = self._measure_graph_z(target, result)
+        else:
+            raise ValueError("You can only measure in {X,Y,Z}")
+
+        # Flip the result if there is a negative phase
+        if phase == -1:
+            result = not result
+
+        return int(result)
 
     # Shorthand Gate Methods
     def h(self, target):
@@ -109,16 +219,11 @@ class GraphState(object):
     def z(self, target):
         self._apply_operation(target, 'pz')
 
-    def __repr__(self):
-        s = ""
-        for key in sorted(self.nodes.keys()):
-            s += "{}: {}\t".format(key,
-                                   get_clifford_name(self.nodes[key]['vop']))
-            if self.adj[key]:
-                s += str(tuple(self.adj[key].keys())).replace(" ", "")
-            else:
-                s += "-"
+    def measure_y(self, node):
+        return self._measure(node, "py")
 
-            s += "\n"
+    def measure_z(self, node):
+        return self._measure(node, "pz")
 
-        return s
+    def measure_x(self, node):
+        return self._measure(node, 'px')
--- test.py
+++ test.py
@@ -16,8 +16,35 @@ def create_graph_state():
     return g
 
 
-g = create_graph_state()
-print(g.nodes)
+def test_random_outcomes():
+    """ Testing random behaviour """
+    ones = 0
+    for i in range(1000):
+        g = GraphState(1, vop="hadamard")
+        g.h(0)
+        ones += g._measure(0, "pz")
+    assert 400 < ones < 600, "This is a probabilistic test!"
+
+
+def test_single_qubit_measurements():
+    """ Various simple tests of measurements """
+
+    # Test that measuring |0> in Z gives 0
+    g = GraphState(1, vop="hadamard")
+    assert g._measure(0, "pz") == 0, "Measuring |0> in Z gives 0"
+
+    # Test that measuring |1> in Z gives 1
+    g = GraphState(1, vop="hadamard")
+    g.x(0)
+    assert g._measure(0, "pz") == 1, "Measuring |1> in Z gives 1"
+
+    # Test that measuring |+> in X gives 0
+    g = GraphState(1, vop="hadamard")
+    g.h(0)
+    assert g._measure(0, "px") == 0
+    assert g._measure(0, "px") == 0, "Measuring |+> in X gives 0"
+    g.z(0)
+    assert g._measure(0, "px") == 1, "Measuring |-> in X gives 1"
 
 
 def test_local_complementation():
@@ -58,3 +85,5 @@ def test_cz():
 test_local_complementation()
 test_remove_vop()
 test_cz()
+test_single_qubit_measurements()
+test_random_outcomes()
